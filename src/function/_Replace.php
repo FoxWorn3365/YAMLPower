de6f559fef38c1928adebb16898dc3b0172a143a
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2023-08-20 19:27:27 +0200

    Create the _Replace.php function

---
+++ src/function/_Replace.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FoxWorn3365\YAMLPower\function;
+
+final class _Replace {
+    public static function execute(object $var, array $args) : object {
+        if (@$var->{$args[1]} !== null) {
+            $string = $var->{$args[1]};
+            foreach ($var as $name => $value) {
+                $string = str_replace($value, '{'.$name.'}', $string);
+            }
+        }
+        $var->{$args[1]} = $string;
+    }
+}
